815c5b7c538fbea14c5073f12de095c3e32829b6
sin3107 <s0pp3107@gmail.com>
2021-01-15 14:30:36 +0900

    친구 설정 수정

--- server/routes/v1/friend.js
+++ server/routes/v1/friend.js
@@ -486,8 +486,8 @@ router.post('/settings', async (req, res) => {
     let result
 
     const params = [
-        {key: 'blind', type: 'num', optional: true, update: true},
-        {key: 'auto', type: 'num', optional: true, update: true}
+        {key: 'blind', value: 'blind', type: 'num', optional: true, update: true},
+        {key: 'auto', value: 'auto', type: 'num', optional: true, update: true}
     ]
 
     try{
